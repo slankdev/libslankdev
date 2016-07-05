319a0928953ff72b9a6f8b94e69cc4a7e6e0da21
slank-arch <slank.dev@gmail.com>
2016-07-05 19:11:02 +0900

    [CLEAN] some codes edited at 2016.07.05-19:11

--- sample/send_pkt/main.cc
+++ sample/send_pkt/main.cc
@@ -3,11 +3,14 @@
 #include <slankdev.h>
 int main(int argc, char** argv)
 {
-    if (argc < 2) {
-        fprintf(stderr, "Usage: %s interface \n", argv[0]);
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s interface count\n", argv[0]);
         return -1;
     }
     slankdev::safe_intfd fd;
     fd.open_if(argv[1]);
-    fd.write(slankdev::raw, sizeof(slankdev::raw));
+
+    slankdev::hexdump("send packet", slankdev::raw, sizeof(slankdev::raw));
+    for (int i=0; i<atoi(argv[2]); i++)
+        fd.write(slankdev::raw, sizeof(slankdev::raw));
 }
