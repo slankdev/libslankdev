d40789fadc275e12685582031f2afa0dca69d746
slank@vagrant <vim>
2016-07-31 07:23:49 +0000

    [ADD] unsafe_singleton1 (templating maybe unsafe?..)

--- include/slankdev/singleton.h
+++ include/slankdev/singleton.h
@@ -29,6 +29,57 @@ public:
 
 
 
+// template <typename T>
+// class unsafe_singleton {
+// protected:
+//     static bool _inited;
+//     unsafe_singleton() {}
+//     ~unsafe_singleton() {}
+//     unsafe_singleton(const unsafe_singleton&) = delete;
+//     unsafe_singleton& operator=(const unsafe_singleton&) = delete;
+//
+// public:
+//     static T& _instance()
+//     {
+//         static T t;
+//         return t;
+//     }
+//     template <typename... Arg>
+//     static T& instance(Arg... args)
+//     {
+//         T& t = _instance();
+//         if (!_inited) {
+//             t.init(args...);
+//             _inited = true;
+//         }
+//         return t;
+//     }
+//     bool inited()
+//     {
+//         return _inited;
+//     }
+// };
+// template <typename T>
+// bool unsafe_singleton<T>::_inited = false;
+//
+//
+// class test : public unsafe_singleton<test> {
+//     friend unsafe_singleton<test>;
+//     private:
+//     test() {puts("cnst");}
+//     ~test() {puts("dst");}
+//     void init()         {puts("init(void)");}
+//     void init(int)      {puts("init(int)");};
+//     void init(double, int) {puts("init(double, int)");};
+// };
+//
+//
+// int main()
+// {
+//     test& ada = test::instance(1.1, 2);
+//     test& t2a = test::instance();
+// }
+
 
 
 } /* namespace slankdev */
