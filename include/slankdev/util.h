2fdc1044485b559add71e7f0cb7d61276c3adc3e
slank@cybozu <slank.dev@gmail.com>
2016-10-05 13:00:28 +0900

    [CLEAN] some codes edited at 2016.10.05-13:00

--- include/slankdev/util.h
+++ include/slankdev/util.h
@@ -14,6 +14,8 @@ uint16_t checksum(const void* data, size_t len);
 void hexdump(const char* title, const void* buffer, size_t bufferlen);
 void bindump(const char* title, const void* buffer, size_t bufferlen);
 
+uint16_t htons(uint16_t n);
+uint16_t ntohs(uint16_t n);
 
 
 void clear_screen();
--- sample/AFINET_SOCKDGRAM_SEND/main.cc
+++ sample/AFINET_SOCKDGRAM_SEND/main.cc
@@ -1,29 +1,23 @@
 
 #include <stdio.h>
 #include <slankdev.h>
-
-namespace s = slankdev;
+#include <arpa/inet.h>
+using namespace slankdev;
 
 
 void sendpacket_l3(const void* buf, size_t buflen,
         const char* device, struct sockaddr_in* dst, size_t dstlen)
 {
-    s::socketfd fd;
-    fd.socket(AF_INET, SOCK_RAW, 255);
+    socketfd fd;
+    fd.socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
 
     struct ifreq ifr;
     memset(&ifr, 0, sizeof ifr);
     snprintf(ifr.ifr_name, sizeof(ifr.ifr_name), "%s", device);
     fd.ioctl(SIOCGIFINDEX, &ifr);
 
-    uint32_t val = 0;
-    fd.setsockopt(IPPROTO_IP, IP_HDRINCL, &val, sizeof(val));
-    val = 1;
-    fd.setsockopt(IPPROTO_IP, IP_MTU_DISCOVER, &val, sizeof(val));
-    fd.setsockopt(SOL_SOCKET, SO_BINDTODEVICE, &ifr, sizeof ifr);
-
     fd.sendto(buf, buflen, 0, (struct sockaddr*)dst, dstlen);
-    printf("send packet\n");
+    printf("send packet. len=%zd\n", buflen);
 }
 
 
@@ -35,15 +29,16 @@ int main(int argc, char** argv)
 
     }
 
+    size_t datalen = 10000;
     uint8_t buf[100000];
     memset(buf, 0xee, sizeof buf);
-    size_t data_len = 10000;
 
     struct sockaddr_in dst;
     memset(&dst, 0, sizeof dst);
     dst.sin_family = AF_INET;
-    dst.sin_port = s::htons(1111);
+    dst.sin_port = ::htons(1111);
     dst.sin_addr.s_addr = inet_addr(argv[2]);
 
-    sendpacket_l3(buf, data_len, argv[1], &dst, sizeof(dst));
+    sendpacket_l3(buf, datalen, argv[1], &dst, sizeof(dst));
 }
+
