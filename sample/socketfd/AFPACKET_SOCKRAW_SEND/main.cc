6d80a54108349fa7d591b0f545cf72e251f8495c
slankdev <slank.dev@gmail.com>
2017-01-15 09:06:44 +0900

    [UPDATE] socketfd sample

--- sample/socketfd/AFPACKET_SOCKRAW_SEND/main.cc
+++ sample/socketfd/AFPACKET_SOCKRAW_SEND/main.cc
@@ -3,6 +3,9 @@
 #include <slankdev/socketfd.h>
 #include <slankdev/util.h>
 using namespace slankdev;
+#define CONST_DEV true
+#define CONST_DEVNAME "enp2s0f0"
+#define CONST_CNT  10
 
 
 uint8_t arp_raw[] = {
@@ -14,23 +17,47 @@ uint8_t arp_raw[] = {
     0x00, 0x00 };
 
 
-
-int main(int argc, char** argv)
+int main_normal(int argc, char** argv)
 {
-	if (argc < 2) {
-		fprintf(stderr, "Usage: %s ifname\n", argv[0]);
-		return -1;
-	}
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s ifname\n", argv[0]);
+        return -1;
+    }
 
     socketfd fd;
     fd.open_if(argv[1]);
 
     hexdump("Sending", arp_raw, sizeof(arp_raw));
 
-	if (argc > 2) {
-		for (int i=0; i<atoi(argv[2]); i++)
-			fd.write(arp_raw, sizeof(arp_raw));
-	} else {
-		fd.write(arp_raw, sizeof(arp_raw));
-	}
+    if (argc > 3) {
+        for (int i=0; i<atoi(argv[2]); i++)
+            fd.write(arp_raw, sizeof(arp_raw));
+    } else {
+        fd.write(arp_raw, sizeof(arp_raw));
+    }
 }
+
+int main_const_dev(int argc, char** argv)
+{
+    socketfd fd;
+    fd.open_if(CONST_DEVNAME);
+
+    hexdump("Sending", arp_raw, sizeof(arp_raw));
+
+    if (argc > 2) {
+        for (int i=0; i<atoi(argv[1]); i++)
+            fd.write(arp_raw, sizeof(arp_raw));
+    } else {
+        for (int i=0; i<CONST_CNT; i++)
+            fd.write(arp_raw, sizeof(arp_raw));
+    }
+
+}
+
+int main(int argc, char** argv)
+{
+    if (CONST_DEV) return main_const_dev(argc, argv);
+    else           return main_normal(argc, argv);
+}
+
+
