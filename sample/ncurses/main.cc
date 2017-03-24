b53b7517673737738244c9329192dc9d776ed2ab
Hiroki SHIROKURA @slankdev <slank.dev@gmail.com>
2017-03-24 18:31:58 +0900

    WIP

--- sample/ncurses/main.cc
+++ sample/ncurses/main.cc
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <slankdev/string.h>
+#include <slankdev/poll.h>
 #include "TuiFrontend.h"
 
 
@@ -23,9 +24,20 @@ int	main(int argc, char** argv)
   func(fe);
 
   fe.refresh();
+
+  struct pollfd fds[2];
+  memset(fds, 0, sizeof(fds));
+  fds[0].fd = fileno(stdin);
+  fds[0].events = POLLIN;
+  fds[1].fd = -1;
+  fds[1].events = POLLIN;
+
 	while (1) {
-    fe.key_input(wgetch(stdscr));
-    fe.refresh();
+    slankdev::poll(fds, sizeof(fds)/sizeof(fds[0]), 100);
+    if (fds[0].revents & POLLIN) {
+      fe.key_input(wgetch(stdscr));
+      fe.refresh();
+    }
 	}
 	endwin();
 }
