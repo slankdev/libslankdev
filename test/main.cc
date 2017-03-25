37200c4bcbf6f495e149b7e2d10a9ace4c157c48
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-03-25 16:49:32 +0900

    Fix: travis conf

---
+++ test/main.cc
@@ -0,0 +1,51 @@
+
+
+#include <slankdev/asciicode.h>
+#include <slankdev/exception.h>
+#include <slankdev/filefd.h>
+#include <slankdev/filelogger.h>
+#include <slankdev/intfd.h>
+#include <slankdev/log.h>
+#include <slankdev/matrix.h>
+#include <slankdev/ncurses.h>
+#include <slankdev/net_header.h>
+#include <slankdev/prototype.h>
+#include <slankdev/singlethon.h>
+#include <slankdev/socketfd.h>
+#include <slankdev/string.h>
+#include <slankdev/system.h>
+#include <slankdev/telnet.h>
+#include <slankdev/util.h>
+#include <slankdev/vty_command.h>
+#include <slankdev/vty.h>
+#include <slankdev/vty_keyfunc.h>
+#include <slankdev/vty_shell.h>
+
+/*
+ * For Double include check
+ */
+#include <slankdev/asciicode.h>
+#include <slankdev/exception.h>
+#include <slankdev/filefd.h>
+#include <slankdev/filelogger.h>
+#include <slankdev/intfd.h>
+#include <slankdev/log.h>
+#include <slankdev/matrix.h>
+#include <slankdev/ncurses.h>
+#include <slankdev/net_header.h>
+#include <slankdev/prototype.h>
+#include <slankdev/singlethon.h>
+#include <slankdev/socketfd.h>
+#include <slankdev/string.h>
+#include <slankdev/system.h>
+#include <slankdev/telnet.h>
+#include <slankdev/util.h>
+#include <slankdev/vty_command.h>
+#include <slankdev/vty.h>
+#include <slankdev/vty_keyfunc.h>
+#include <slankdev/vty_shell.h>
+
+int main() {}
+
+
+
