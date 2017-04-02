96470a1bcc25d2269e1c973105893bcb971ead2c
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-04-02 13:21:39 +0900

    Refact: slankdev/pcap

--- slankdev/pcap.h
+++ slankdev/pcap.h
@@ -1,10 +1,38 @@
 
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017 Susanoo G
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/**
+ * @file   slankdev/pcap.h
+ * @brief  pcap wrapper
+ * @author Hiroki SHIROKURA
+ * @date   2017.4.2
+ */
+
+
 #pragma once
 
 #include <pcap.h>
-#include <slankdev/exception.h>
-#include <slankdev/util.h>
-#include <slankdev/string.h>
 
 namespace slankdev {
 
@@ -15,79 +43,113 @@ class pcap {
   pcap() : handle(nullptr) {}
   virtual ~pcap() { close(); }
 
-  int get_selectable_fd()
-  {
-    int res = pcap_get_selectable_fd(handle);
-    if (res < 0) {
-      throw slankdev::exception("pcap_get_selectable_fd");
-    }
-    return res;
-  }
-  virtual void open_live(const char* ifname)
-  {
-    if (handle)
-      throw slankdev::exception("pcap: exist open discriptor");
-
-    char errbuf[PCAP_ERRBUF_SIZE];
-    handle = pcap_open_live(ifname, BUFSIZ, 0, 0, errbuf);
-    if (!handle) {
-      std::string errstr;
-      errstr = slankdev::fs("pcap_open_live: %s", errbuf);
-      throw slankdev::exception(errstr.c_str());
-    }
-  }
-  virtual void open_offline(const char* filepath)
-  {
-    if (handle)
-      throw slankdev::exception("pcap: exist open discriptor");
-
-    char errbuf[PCAP_ERRBUF_SIZE];
-    handle = pcap_open_offline(filepath, errbuf);
-    if (!handle) {
-      std::string errstr;
-      errstr = slankdev::fs("pcap_open_offline: %s", errbuf);
-      throw slankdev::exception(errstr.c_str());
-    }
-  }
-  virtual void close()
-  {
-    if (handle) {
-      pcap_close(handle);
-      handle = nullptr;
-    }
-  }
-  void next()
-  {
-    struct pcap_pkthdr header;
-    const u_char *packet = pcap_next(handle, &header);
-    if (packet == nullptr) {
-      throw slankdev::exception("pcap_next: recvmiss?");
-    }
-    recv(packet, &header);
-  }
-  virtual void recv(const void* ptr, struct pcap_pkthdr* header)
-  {
-    slankdev::hexdump("Userfunction", ptr, header->len);
-  }
-  void setfilter(struct bpf_program *fp)
-  {
-    int ret = pcap_setfilter(handle, fp);
-    if (ret < 0) {
-      std::string errstr = pcap_geterr(handle);
-      throw slankdev::exception(errstr.c_str());
-    }
-  }
+  int get_selectable_fd();
+  virtual void open_live(const char* ifname);
+  virtual void open_offline(const char* filepath);
+  void close();
+  void next();
+  virtual void recv(const void* ptr, struct pcap_pkthdr* header);
+  void setfilter(struct bpf_program *fp);
   void compile(struct bpf_program *fp,
-      const char *str, int optimize, bpf_u_int32 netmask)
-  {
-    int ret = pcap_compile(handle, fp, str, optimize, netmask);
-    if (ret < 0) {
-      std::string errstr = pcap_geterr(handle);
-      throw slankdev::exception(errstr.c_str());
-    }
-  }
+      const char *str, int optimize, bpf_u_int32 netmask);
+
 
 };
 
 
 } /* namespace slankadev */
+
+
+
+
+
+/*
+ * Function Implementation
+ */
+
+#include <slankdev/exception.h>
+#include <slankdev/util.h>
+#include <slankdev/string.h>
+
+
+namespace slankdev {
+
+inline int pcap::get_selectable_fd()
+{
+  int res = pcap_get_selectable_fd(handle);
+  if (res < 0) {
+    throw slankdev::exception("pcap_get_selectable_fd");
+  }
+  return res;
+}
+inline void pcap::open_live(const char* ifname)
+{
+  if (handle)
+    throw slankdev::exception("pcap: exist open discriptor");
+
+  char errbuf[PCAP_ERRBUF_SIZE];
+  handle = pcap_open_live(ifname, BUFSIZ, 0, 0, errbuf);
+  if (!handle) {
+    std::string errstr;
+    errstr = slankdev::fs("pcap_open_live: %s", errbuf);
+    throw slankdev::exception(errstr.c_str());
+  }
+}
+inline void pcap::open_offline(const char* filepath)
+{
+  if (handle)
+    throw slankdev::exception("pcap: exist open discriptor");
+
+  char errbuf[PCAP_ERRBUF_SIZE];
+  handle = pcap_open_offline(filepath, errbuf);
+  if (!handle) {
+    std::string errstr;
+    errstr = slankdev::fs("pcap_open_offline: %s", errbuf);
+    throw slankdev::exception(errstr.c_str());
+  }
+}
+inline void pcap::close()
+{
+  if (handle) {
+    pcap_close(handle);
+    handle = nullptr;
+  }
+}
+inline void pcap::next()
+{
+  struct pcap_pkthdr header;
+  const u_char *packet = pcap_next(handle, &header);
+  if (packet == nullptr) {
+    throw slankdev::exception("pcap_next: recvmiss?");
+  }
+  recv(packet, &header);
+}
+inline void pcap::recv(const void* ptr, struct pcap_pkthdr* header)
+{
+  slankdev::hexdump("Userfunction", ptr, header->len);
+}
+inline void pcap::setfilter(struct bpf_program *fp)
+{
+  int ret = pcap_setfilter(handle, fp);
+  if (ret < 0) {
+    std::string errstr = pcap_geterr(handle);
+    throw slankdev::exception(errstr.c_str());
+  }
+}
+inline void pcap::compile(struct bpf_program *fp,
+    const char *str, int optimize, bpf_u_int32 netmask)
+{
+  int ret = pcap_compile(handle, fp, str, optimize, netmask);
+  if (ret < 0) {
+    std::string errstr = pcap_geterr(handle);
+    throw slankdev::exception(errstr.c_str());
+  }
+}
+
+} /* namespace slankadev */
+
+
+
+
+
+
