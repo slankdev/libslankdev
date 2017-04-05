be632b623306926a14733255b23f798183223ea9
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-04-05 22:10:31 +0900

    Add: example/hash/rss

---
+++ example/hash/rss/gen.py
@@ -0,0 +1,27 @@
+
+
+
+print { "dst address",
+        hex(161),
+        hex(142),
+        hex(100),
+        hex(80),
+        }
+
+print { "src address",
+        hex(66),
+        hex(9),
+        hex(149),
+        hex(187),
+        }
+
+print { "dst port",
+        (1766),
+        hex(1766)
+        }
+
+print { "src port",
+        (2794),
+        hex(2794)
+        }
+
