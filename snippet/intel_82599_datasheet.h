5ea8b4d3c9db84d5c041156b8a8caca2e58acb82
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-04-06 11:20:26 +0900

    Add: snippet/intel_82599_datasheet.h

---
+++ snippet/intel_82599_datasheet.h
@@ -0,0 +1,9 @@
+
+
+uint32_t src_addr = 0x420995bb;
+uint32_t dst_addr = 0xa18e6450;
+uint16_t sport    = 2794      ;
+uint16_t dport    = 1766      ;
+
+uint32_t ipv4_only = 0x323e8fc2;
+uint32_t ipv4_tcp  = 0x51ccc178;
