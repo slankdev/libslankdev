43d9d62358581aa71c1b7a1629e683fed1533586
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-05-16 23:57:36 +0900

    Add: slankdev/types.h

---
+++ slankdev/types.h
@@ -0,0 +1,23 @@
+
+#pragma once
+
+typedef signed char  int8_t;
+typedef signed short int16_t;
+typedef signed int   int32_t;
+typedef signed long  int64_t;
+
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+typedef unsigned long  uint64_t;
+
+typedef uint64_t u64;
+typedef uint32_t u32;
+typedef uint16_t u16;
+typedef uint8_t  u8;
+
+typedef int64_t i64;
+typedef int32_t i32;
+typedef int16_t i16;
+typedef int8_t  i8;
+
