9b99179692a8b7eb61b5911536284c43be760c56
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-05-25 11:02:07 +0900

    Update: example

--- example/dpdk/l2fwd-pipeline/main.cc
+++ example/dpdk/l2fwd-pipeline/main.cc
@@ -2,19 +2,52 @@
 #include <stdio.h>
 #include <slankdev/extra/dpdk.h>
 
-int l2fwd(void*)
+
+struct wk_arg {
+  struct rte_ring* src;
+  struct rte_ring* dst;
+};
+
+int wk(void* arg)
 {
+  printf("launched wk on lcore%u \n", rte_lcore_id());
+  struct wk_arg* wk = reinterpret_cast<wk_arg*>(arg);
   while (true) {
     constexpr size_t BURSTSZ = 32;
-    rte_mbuf* mbufs[BURSTSZ];
-
-    constexpr size_t nb_q = 1;
-    size_t nb_recv = rte_eth_rx_burst(0, 0, mbufs, BURSTSZ);
-    if (nb_recv == 0) continue;
-    size_t nb_send = rte_eth_tx_burst(1, 0, mbufs, nb_recv);
-    if (nb_send < nb_recv)
-      slankdev::rte_pktmbuf_free_bulk(&mbufs[nb_send], nb_recv-nb_send);
+    struct rte_mbuf* mbufs[BURSTSZ];
+    size_t nb_deq = rte_ring_dequeue_bulk(wk->src, (void**)mbufs, sizeof(mbufs), nullptr);
+    size_t nb_enq = rte_ring_enqueue_bulk(wk->dst, (void**)mbufs, nb_deq, nullptr);
+    if (nb_enq < nb_deq)
+      slankdev::rte_pktmbuf_free_bulk(&mbufs[nb_enq], nb_deq-nb_enq);
   }
+  return 0;
+}
+
+int l2fwd(void* arg)
+{
+  printf("launched l2fwd on lcore%u \n", rte_lcore_id());
+  struct wk_arg* wk = reinterpret_cast<wk_arg*>(arg);
+  size_t nb_ports = rte_eth_dev_count();
+  while (true) {
+    for (size_t pid=0; pid<nb_ports; pid++) {
+      constexpr size_t BURSTSZ = 32;
+      rte_mbuf* mbufs[BURSTSZ];
+
+      constexpr size_t nb_q = 1;
+      size_t nb_recv = rte_eth_rx_burst(pid, 0, mbufs, BURSTSZ);
+      if (nb_recv == 0) continue;
+
+      size_t nb_enq = rte_ring_enqueue_bulk(wk->src, (void**)mbufs, nb_recv, nullptr);
+      if (nb_enq < nb_recv)
+        slankdev::rte_pktmbuf_free_bulk(&mbufs[nb_enq], nb_recv-nb_enq);
+
+      size_t nb_deq = rte_ring_dequeue_bulk(wk->dst, (void**)mbufs, sizeof(mbufs), nullptr);
+      size_t nb_send = rte_eth_tx_burst(pid^1, 0, mbufs, nb_deq);
+      if (nb_send < nb_deq)
+        slankdev::rte_pktmbuf_free_bulk(&mbufs[nb_send], nb_deq-nb_send);
+    }
+  }
+  return 0;
 }
 
 int main(int argc, char** argv)
@@ -30,7 +63,29 @@ int main(int argc, char** argv)
     slankdev::port_configure(i, 1, 1, &port_conf, mp);
   }
 
-  rte_eal_remote_launch(l2fwd, nullptr, 1);
+  struct rte_ring* r0 = slankdev::ring_alloc("WK0", 1024);
+  struct rte_ring* r1 = slankdev::ring_alloc("WK1", 1024);
+  struct rte_ring* r2 = slankdev::ring_alloc("WK2", 1024);
+  struct rte_ring* r3 = slankdev::ring_alloc("WK3", 1024);
+  struct rte_ring* r4 = slankdev::ring_alloc("WK4", 1024);
+  struct rte_ring* r5 = slankdev::ring_alloc("WK5", 1024);
+  struct rte_ring* r6 = slankdev::ring_alloc("WK6", 1024);
+
+  struct wk_arg fwd_arg = {r0, r6};
+  struct wk_arg wk0_arg = {r0, r1};
+  struct wk_arg wk1_arg = {r1, r2};
+  struct wk_arg wk2_arg = {r2, r3};
+  struct wk_arg wk3_arg = {r3, r4};
+  struct wk_arg wk4_arg = {r4, r5};
+  struct wk_arg wk5_arg = {r5, r6};
+
+  rte_eal_remote_launch(l2fwd, &fwd_arg, 1);
+  rte_eal_remote_launch(wk   , &wk0_arg, 2);
+  rte_eal_remote_launch(wk   , &wk1_arg, 3);
+  rte_eal_remote_launch(wk   , &wk2_arg, 4);
+  rte_eal_remote_launch(wk   , &wk3_arg, 5);
+  rte_eal_remote_launch(wk   , &wk4_arg, 6);
+  rte_eal_remote_launch(wk   , &wk5_arg, 7);
   rte_eal_mp_wait_lcore();
 }
 
