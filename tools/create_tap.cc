26f47f7e8234684d982fb312fd0ffbd49b7e42fb
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-08-06 14:10:32 +0000

    2017.08.06-14:10

---
+++ tools/create_tap.cc
@@ -0,0 +1,17 @@
+
+
+#include <stdio.h>
+#include <slankdev/tuntap.h>
+
+int main(int argc, char** argv)
+{
+  if (argc < 2) {
+    fprintf(stderr, "Usage: %s ifname\n", argv[0]);
+    return -1;
+  }
+
+  slankdev::tap tap(argv[1]);
+  while (1) {}
+}
+
+
