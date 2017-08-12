e9d8d7116da4adf27620beb2c91aac165ecbd555
Hiroki SHIROKURA <slank.dev@gmail.com>
2017-08-12 12:55:44 +0000

    2017.08.12-12:55

--- slankdev/socketfd.h
+++ slankdev/socketfd.h
@@ -88,6 +88,7 @@ class socketfd : public safe_intfd {
 };
 
 std::string inaddr2str(uint32_t inaddr);
+std::string macaddr2str(uint8_t macaddr[6]);
 
 } /* namespace slankdev */
 
@@ -308,6 +309,13 @@ inline std::string inaddr2str(uint32_t inaddr)
   U.u32 = inaddr;
   std::string str = slankdev::format("%02x.%02x.%02x.%02x",
       U.u8[0], U.u8[1], U.u8[2], U.u8[3]);
+  return str;
+}
+
+inline std::string macaddr2str(uint8_t macaddr[6])
+{
+  std::string str = slankdev::format("%02x:%02x:%02x:%02x:%02x:%02x",
+      macaddr[0], macaddr[1], macaddr[2], macaddr[3], macaddr[4], macaddr[5]);
   return str;
 }
 
