835325ffbb20f8e83f0cabb61aee416cf4b1603a
Hiroki SHIROKURA <slank.dev@gmail.com>
2018-02-04 20:40:22 +0900

    2018.02.04-20:40

---
+++ mk/rules.mk
@@ -0,0 +1,17 @@
+
+.SUFFIXES: .out .c .cc .o .hS
+.cc.o:
+	@echo "CXX $@"
+	@$(CXX) $(CXXFLAGS) -c $< -o $@
+
+.c.o:
+	@echo "CC $@"
+	@$(CC) $(CFLAGS) -c $< -o $@
+
+# $(TARGET): $(CXXOBJS) $(COBJS)
+# 	@echo "LD $@"
+# 	@$(CXX) $(CXXFLAGS) -o $(TARGET) $(CXXOBJS) $(COBJS) $(LDFLAGS)
+
+
+
+
